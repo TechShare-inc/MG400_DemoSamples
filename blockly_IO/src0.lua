aca929ff4b0463496fab6a937968c2ef0de87314
kskJin <82190248+kskJin@users.noreply.github.com>
2021-10-08 15:56:23 +0900

    Add Copyright.

--- blockly_IO/src0.lua
+++ blockly_IO/src0.lua
@@ -1,3 +1,4 @@
+-- Copyright © 2021 TechShare Inc. All Rights Reserved.
 -- DI_16に接続したスイッチからの入力時、DO_16に接続したLEDに出力・+y方向への移動。DO_14に接続したスイッチからの入力時、DO_15に接続したLEDに出力・-y方向への移動。DI_15に接続したスイッチから入力時、DO_01に接続したエアポンプのON・OFF切り替え。
 -- DO_01に接続したエアポンプの状態を変数とする(ON時=1,OFF時=0)
 airpomp = 0
